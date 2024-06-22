a171ff589b56b811b69e635f33411501ec1cd4ad
stephane <stephanekoffi@live.fr>
2024-06-22 12:21:41 +0200

    commande partie 1

---
+++ src/Entity/Carrier.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarrierRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: CarrierRepository::class)]
+class Carrier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Assert\NotBlank(message: "Le nom du transporteur est obligatoire.")]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'Le nom du transporteur ne doit pas dépasser {{ limit }} caractères.',
+    )]
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[Assert\NotBlank(message : " La description est obligatoire.")]
+    #[Assert\Length(
+        max: 500,
+        maxMessage: 'La description ne doit pas dépasser {{ limit }} caractères'
+    )]
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[Assert\NotBlank(message : "La prix est obligatoire.")]
+    #[Assert\Positive(message : "Le prix de la livraison doit être un nombre positif.")]
+    #[Assert\Regex (
+        pattern: "/^\d+(\.\d{1,2})?$/",
+        message: "Le prix de la livraison doit être un entier ou un nombre décimal avec au plus deux décimales."
+    )]
+    #[Assert\Range(
+        min: 0.1,
+        max: 999999,
+        notInRangeMessage: 'Le prix de la livraison doit être compris entre {{ mini }} euros {{ max }} euros',
+    )]
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
